827faf4bcda29fad671b307061600b50ce175030
Murphy573 <lipfd@yonyou.com>
2020-07-15 12:11:16 +0800

    feat: 修改date.js

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,4 +1,18 @@
 /**
+ * 将任意时间格式转换为Date对象
+ * @param {Number|Date|String} time 时间
+ * @returns Date
+ */
+export function toDate (time) {
+  if (!time) {
+    return new Date();
+  };
+  if (typeof time === 'string') {
+    time = time.replace(/-/g, '/');
+  }
+  return new Date(time);
+}
+/**
  * 日期转换
  * @param {Number|Date|String} time 时间
  * @param {String} ftm 转换格式
@@ -12,11 +26,10 @@
  */
 export function formatDate (time, fmt = 'YYYY-MM-DD') {
   if (!time) return '';
-  if (typeof time === 'string') {
-    time = time.replace(/-/g, '/');
-  }
-  const _time = new Date(time);
-  const o = {
+
+  let _time = toDate(time);
+
+  let o = {
     'M+': _time.getMonth() + 1, // 月份
     'D+': _time.getDate(), // 日
     'H+': _time.getHours(), // 小时
@@ -31,7 +44,7 @@ export function formatDate (time, fmt = 'YYYY-MM-DD') {
       (_time.getFullYear() + '').substr(4 - RegExp.$1.length)
     );
   }
-  for (const k in o) {
+  for (let k in o) {
     if (new RegExp('(' + k + ')').test(fmt)) {
       fmt = fmt.replace(
         RegExp.$1,
@@ -47,7 +60,7 @@ export function formatDate (time, fmt = 'YYYY-MM-DD') {
  * @param {String | Number | Date} time 日期
  */
 export function getCurrentMonthDays (time) {
-  const _time = new Date(time);
+  let _time = new Date(time);
   return new Date(_time.getFullYear(), _time.getMonth() + 1, 0).getDate();
 }
 
@@ -60,7 +73,7 @@ export function getDiffDays (date1, date2) {
   if (!date1 || !date2) {
     return -1;
   }
-  const _d1 = new Date(date1).getTime(),
+  let _d1 = new Date(date1).getTime(),
     _d2 = new Date(date2).getTime();
   const ONE_DAY = 24 * 60 * 60 * 1000; // 一天的毫秒数
   return Math.abs(Math.floor((_d2 - _d1) / ONE_DAY));
@@ -77,4 +90,17 @@ export function getWeekDay (time, prefix = '星期') {
   }
   const week = ['日', '一', '二', '三', '四', '五', '六'];
   return `${prefix}${week[new Date(time).getDay()]}`;
+}
+
+/**
+ * 判断时间1是否在时间2之前
+ * @param {Date|Number|String} time1 时间1
+ * @param {Date|Number|String} time2 时间2
+ * @returns Boolean
+ */
+export function isBefore (time1, time2) {
+  if (!time1 || !time2) return false;
+  time1 = toDate(time1).getTime();
+  time2 = toDate(time2).getTime();
+  return time1 < time2;
 }
